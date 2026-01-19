ba0869b2130b3103197231a5cac4424b23fa7e2e
Enrique <edavalos@ucsc.edu>
2026-01-18 23:56:28 -0800

    Added success and code fields to Response
    Also added constructor

--- src/main/java/com/mtxrii/file/mtxfile/api/model/Response.java
+++ src/main/java/com/mtxrii/file/mtxfile/api/model/Response.java
@@ -1,4 +1,11 @@
 package com.mtxrii.file.mtxfile.api.model;
 
 public class Response {
+    private final boolean success;
+    private final int code;
+
+    public Response(boolean success, int code) {
+        this.success = success;
+        this.code = code;
+    }
 }
