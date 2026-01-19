9e89dfa5de3b6ee13d728085a7ca29eed1dd48e0
Enrique <edavalos@ucsc.edu>
2026-01-18 23:17:35 -0800

    Initial commit

---
+++ src/main/java/com/mtxrii/file/mtxfile/MtxFileApplication.java
@@ -0,0 +1,13 @@
+package com.mtxrii.file.mtxfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtxFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtxFileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mtxrii/file/mtxfile/MtxFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtxrii.file.mtxfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtxFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
