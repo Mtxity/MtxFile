c53cfea545bbb1e66ea334515cf7525668b31b4c
Enrique <edavalos@ucsc.edu>
2026-01-18 23:51:39 -0800

    Added toJson() in Main
    Utility method to serialize objects to JSON using Gson

--- src/main/java/com/mtxrii/file/mtxfile/MtxFileApplication.java
+++ src/main/java/com/mtxrii/file/mtxfile/MtxFileApplication.java
@@ -1,13 +1,18 @@
 package com.mtxrii.file.mtxfile;
 
+import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MtxFileApplication {
+    private static final Gson GSON = new Gson();
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(MtxFileApplication.class, args);
     }
 
+    public static String toJson(Object obj) {
+        return GSON.toJson(obj);
+    }
 }
