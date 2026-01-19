1d214bbfbd55a8d1138274df6095e7a79b9950f1
Enrique <edavalos@ucsc.edu>
2026-01-19 00:05:43 -0800

    Added file upload request param to handleUpload() in UploadController

--- src/main/java/com/mtxrii/file/mtxfile/api/controller/UploadController.java
+++ src/main/java/com/mtxrii/file/mtxfile/api/controller/UploadController.java
@@ -4,7 +4,9 @@ import com.mtxrii.file.mtxfile.api.service.UploadService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class UploadController {
@@ -15,9 +17,9 @@ public class UploadController {
     }
 
     @PostMapping(path = "/upload", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> handleUpload() {
+    public ResponseEntity<String> handleUpload(@RequestParam("file") MultipartFile file) {
         return ResponseEntity
                 .status(200)
-                .body("{\"test\": true}");
+                .body("{\"fileName\": \"" + file.getOriginalFilename() + "\"}");
     }
 }
