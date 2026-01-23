11b5ac5973a09a71c507b0efc55aad1feddbff7d
Enrique <edavalos@ucsc.edu>
2026-01-23 02:12:31 -0800

    Moved file metadata into nested map in ReadContentsResponse

--- src/main/java/com/mtxrii/file/mtxfile/api/model/ReadContentsResponse.java
+++ src/main/java/com/mtxrii/file/mtxfile/api/model/ReadContentsResponse.java
@@ -2,29 +2,34 @@ package com.mtxrii.file.mtxfile.api.model;
 
 import jakarta.annotation.Nullable;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class ReadContentsResponse extends Response {
     public final String fileName;
     public final String contents;
-    public final long fileSizeBytes;
-    public final String fileContentType;
-    public final Map<String, Object> fileMetadata; // @TODO: Refactor this to have all metadata in nested classes (file and request meta)
+    public final Map<String, Object> fileMeta;
 
     public ReadContentsResponse(boolean success, int code, String fileName, long fileSizeBytes, String fileContentType, String contents, @Nullable Map<String, Object> metadata) {
         super(success, code);
         this.fileName = fileName;
-        this.fileSizeBytes = fileSizeBytes;
-        this.fileContentType = fileContentType;
         this.contents = contents;
-        this.fileMetadata = metadata;
+        this.fileMeta = new LinkedHashMap<>();
+        this.fileMeta.put("fileSizeBytes", fileSizeBytes);
+        this.fileMeta.put("fileContentType", fileContentType);
+        if (metadata != null) {
+            this.fileMeta.put("extraFileMeta", metadata);
+        }
     }
 
     public ReadContentsResponse(String fileName, long fileSizeBytes, String fileContentType, String contents, @Nullable Map<String, Object> metadata) {
         this.fileName = fileName;
-        this.fileSizeBytes = fileSizeBytes;
-        this.fileContentType = fileContentType;
         this.contents = contents;
-        this.fileMetadata = metadata;
+        this.fileMeta = new LinkedHashMap<>();
+        this.fileMeta.put("fileSizeBytes", fileSizeBytes);
+        this.fileMeta.put("fileContentType", fileContentType);
+        if (metadata != null) {
+            this.fileMeta.put("extraFileMeta", metadata);
+        }
     }
 }
