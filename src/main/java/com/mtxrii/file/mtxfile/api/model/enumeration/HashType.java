d7fb97183cb91df503616fd66035eec187f8eba5
Enrique <edavalos@ucsc.edu>
2026-01-31 02:35:14 -0800

    Added HashType enum

---
+++ src/main/java/com/mtxrii/file/mtxfile/api/model/enumeration/HashType.java
@@ -0,0 +1,22 @@
+package com.mtxrii.file.mtxfile.api.model.enumeration;
+
+public enum HashType {
+    SHA_224("SHA-224"),
+    SHA_256("SHA-256"),
+    SHA_384("SHA-384"),
+    SHA_512("SHA-512"),
+    SHA_512_224("SHA-512/224"),
+    SHA_512_256("SHA-512/256"),
+    MD2("MD2"),
+    MD5("MD5");
+
+    private final String key;
+
+    HashType(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return this.key;
+    }
+}
