026db1ad53173703c6462dbacc7943151ff20e12
Enrique <edavalos@ucsc.edu>
2026-02-02 19:38:27 -0800

    Changed timestamp field type to String in ErrorResponse

--- src/main/java/com/mtxrii/file/mtxfile/api/model/ErrorResponse.java
+++ src/main/java/com/mtxrii/file/mtxfile/api/model/ErrorResponse.java
@@ -3,12 +3,12 @@ package com.mtxrii.file.mtxfile.api.model;
 import java.time.Instant;
 
 public class ErrorResponse extends Response {
-    public final Instant timestamp;
+    public final String timestamp;
     public final String message;
 
     public ErrorResponse(int code, String message) {
         super(false, code);
-        this.timestamp = Instant.now();
+        this.timestamp = Instant.now().toString();
         this.message = message;
     }
 }
