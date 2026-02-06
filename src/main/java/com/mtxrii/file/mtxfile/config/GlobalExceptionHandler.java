50dd6b45da568af5d1a908df6961cdd3157ca356
Enrique <edavalos@ucsc.edu>
2026-02-05 23:17:00 -0800

    Added GlobalExceptionHandler

---
+++ src/main/java/com/mtxrii/file/mtxfile/config/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.mtxrii.file.mtxfile.config;
+
+import com.mtxrii.file.mtxfile.api.model.ErrorResponse;
+import com.mtxrii.file.mtxfile.api.model.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Response> handleIllegalArgumentException(IllegalArgumentException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+}
