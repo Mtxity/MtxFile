f71a2fbf6ae5d1540012e7f75daf7529c4910525
Enrique <edavalos@ucsc.edu>
2026-02-06 22:32:42 -0800

    Added CsvController

---
+++ src/main/java/com/mtxrii/file/mtxfile/api/controller/CsvController.java
@@ -0,0 +1,10 @@
+package com.mtxrii.file.mtxfile.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/csv")
+public class CsvController {
+    private static final String FILE_PARAM = "file";
+}
