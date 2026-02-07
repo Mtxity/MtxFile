3dc2c40f9dce2913f03c90ba8ab86105d4bf6ced
Enrique <edavalos@ucsc.edu>
2026-02-06 23:36:19 -0800

    Added constructor to CsvFequenciesResponse

--- src/main/java/com/mtxrii/file/mtxfile/api/model/CsvFequenciesResponse.java
+++ src/main/java/com/mtxrii/file/mtxfile/api/model/CsvFequenciesResponse.java
@@ -8,4 +8,9 @@ import java.util.Map;
 public class CsvFequenciesResponse extends Response {
     public final String fileName;
     public final Map<String, Map<String, Integer>> valueFrequencies;
+
+    public CsvFequenciesResponse(String fileName, Map<String, Map<String, Integer>> valueFrequencies) {
+        this.fileName = fileName;
+        this.valueFrequencies = valueFrequencies;
+    }
 }
