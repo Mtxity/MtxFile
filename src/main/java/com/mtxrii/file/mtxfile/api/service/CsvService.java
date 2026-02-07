a65b853b405a2ebd53f0166aaf109a8c293ce421
Enrique <edavalos@ucsc.edu>
2026-02-06 22:41:26 -0800

    Added CsvService
    with jsonifyCsv() from ReadService

---
+++ src/main/java/com/mtxrii/file/mtxfile/api/service/CsvService.java
@@ -0,0 +1,38 @@
+package com.mtxrii.file.mtxfile.api.service;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CsvService {
+
+    public List<Map<String, String>> jsonifyCsv(MultipartFile file) throws IOException {
+        try (
+                Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8));
+                CSVParser csvParser = CSVFormat.DEFAULT.withFirstRecordAsHeader().withTrim().parse(reader)
+        ) {
+            List<Map<String, String>> records = new ArrayList<>();
+            for (CSVRecord record : csvParser) {
+                Map<String, String> row = new LinkedHashMap<>();
+                for (String header : csvParser.getHeaderMap().keySet()) {
+                    row.put(header, record.get(header));
+                }
+                records.add(row);
+            }
+            return records;
+        }
+    }
+}
