400f65aa1dcc6b4850e1761c36950908ab38d66b
Enrique <edavalos@ucsc.edu>
2026-02-14 02:17:24 -0800

    Added TODO comment in MtxIoException constructor

--- src/main/java/com/mtxrii/file/mtxfile/exception/MtxIoException.java
+++ src/main/java/com/mtxrii/file/mtxfile/exception/MtxIoException.java
@@ -3,6 +3,7 @@ package com.mtxrii.file.mtxfile.exception;
 import java.io.IOException;
 
 public class MtxIoException extends IOException {
+    // @TODO: Figure out where this is needed
     Exception e;
 
     public MtxIoException(Exception e) {
