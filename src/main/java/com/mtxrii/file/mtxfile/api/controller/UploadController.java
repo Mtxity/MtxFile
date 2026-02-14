33f3ac1d6a04ca0afbfff2e2d98c20ac7d7e2d3a
Enrique <edavalos@ucsc.edu>
2026-02-14 02:22:48 -0800

    Added UploadController with base mapping for file uploads

---
+++ src/main/java/com/mtxrii/file/mtxfile/api/controller/UploadController.java
@@ -0,0 +1,9 @@
+package com.mtxrii.file.mtxfile.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/upload")
+public class UploadController {
+}
