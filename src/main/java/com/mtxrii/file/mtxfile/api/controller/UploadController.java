4fef2e5ac780b848d69f8e1dfbb70c001d45752c
Enrique <edavalos@ucsc.edu>
2026-02-14 02:26:43 -0800

    Added `handleReadContents` endpoint in `UploadController` for processing file uploads

--- src/main/java/com/mtxrii/file/mtxfile/api/controller/UploadController.java
+++ src/main/java/com/mtxrii/file/mtxfile/api/controller/UploadController.java
@@ -1,9 +1,40 @@
 package com.mtxrii.file.mtxfile.api.controller;
 
+import com.mtxrii.file.mtxfile.api.model.ReadContentsResponse;
+import com.mtxrii.file.mtxfile.api.model.Response;
+import com.mtxrii.file.mtxfile.api.service.UploadService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/upload")
 public class UploadController {
+    private static final String FILE_PARAM = "file";
+    private final UploadService uploadService;
+
+    public UploadController(UploadService uploadService) {
+        this.uploadService = uploadService;
+    }
+
+    @PostMapping(
+            value = "/contents",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Response> handleReadContents(
+            @RequestParam(FILE_PARAM) MultipartFile file
+    ) {
+        boolean response = uploadService.uploadFile(file);
+        return ResponseEntity
+                .status(200)
+                .body(new Response(true, 200));
+    }
 }
