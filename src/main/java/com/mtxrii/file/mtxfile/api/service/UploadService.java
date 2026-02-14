018930ecb02c800e0f4f9b1b615b4c60a902842b
Enrique <edavalos@ucsc.edu>
2026-02-14 02:21:33 -0800

    Implemented uploadFile method in UploadService with in-memory storage

--- src/main/java/com/mtxrii/file/mtxfile/api/service/UploadService.java
+++ src/main/java/com/mtxrii/file/mtxfile/api/service/UploadService.java
@@ -1,7 +1,21 @@
 package com.mtxrii.file.mtxfile.api.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class UploadService {
+    private static final Map<String, MultipartFile> UPLOADED_FILES = new ConcurrentHashMap<>();
+
+    public boolean uploadFile(MultipartFile file) {
+        if (UPLOADED_FILES.containsKey(file.getOriginalFilename())) {
+            return false;
+        } else {
+            UPLOADED_FILES.put(file.getOriginalFilename(), file);
+            return true;
+        }
+    }
 }
