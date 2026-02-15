15dfd3ff4815cc303352bb7b4ebdf263463dd5dc
Enrique <edavalos@ucsc.edu>
2026-02-15 02:04:35 -0800

    Updated handleGetContents() in UploadController to use @PathVariable instead of @RequestParam

--- src/main/java/com/mtxrii/file/mtxfile/api/controller/UploadController.java
+++ src/main/java/com/mtxrii/file/mtxfile/api/controller/UploadController.java
@@ -7,6 +7,7 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -44,7 +45,7 @@ public class UploadController {
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     public ResponseEntity<Response> handleGetContents(
-            @RequestParam("fileName") String fileName
+            @PathVariable String fileName
     ) {
         Response uploadContentsResponse = this.uploadService.getUploadedFilePreview(fileName);
         return ResponseEntity
