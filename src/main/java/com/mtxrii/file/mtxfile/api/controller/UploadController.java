d598531f12cd6ee688dbeda3afb81b5e24cd8c71
Enrique <edavalos@ucsc.edu>
2026-02-15 01:20:50 -0800

    Renamed handleReadContents() to handleUploadContents()

--- src/main/java/com/mtxrii/file/mtxfile/api/controller/UploadController.java
+++ src/main/java/com/mtxrii/file/mtxfile/api/controller/UploadController.java
@@ -27,7 +27,7 @@ public class UploadController {
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public ResponseEntity<Response> handleReadContents(
+    public ResponseEntity<Response> handleUploadContents(
             @RequestParam(FILE_PARAM) MultipartFile file
     ) {
         boolean response = uploadService.uploadFile(file);
