c10a25395d86ecc6e9097c298cf91639461302fd
Enrique <edavalos@ucsc.edu>
2026-02-15 01:54:13 -0800

    Added getTruncatedFileContents() to UploadService
    and used it in getUploadedFilePreview()

--- src/main/java/com/mtxrii/file/mtxfile/api/service/UploadService.java
+++ src/main/java/com/mtxrii/file/mtxfile/api/service/UploadService.java
@@ -4,12 +4,20 @@ import com.mtxrii.file.mtxfile.api.model.UploadContentsResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class UploadService {
     private static final Map<String, MultipartFile> UPLOADED_FILES = new ConcurrentHashMap<>();
+    private static final int FILE_CONTENTS_TRUNCATE_SIZE = 300;
+
+    private final ReadService readService;
+
+    public UploadService(ReadService readService) {
+        this.readService = readService;
+    }
 
     public boolean uploadFile(MultipartFile file) {
         String fileName = file.getOriginalFilename().toUpperCase();
@@ -27,11 +35,20 @@ public class UploadService {
             return new UploadContentsResponse(
                     true,
                     fileName,
-                    "To be implemented...",
+                    this.getTruncatedFileContents(file),
                     -1 // @TODO: Implement this
             );
         } else {
             return new UploadContentsResponse(false, fileName, null, -1);
+        }
+    }
+
+    private String getTruncatedFileContents(MultipartFile file) {
+        try {
+            String fileContents = this.readService.readContents(file).getContents();
+            return fileContents.substring(0, FILE_CONTENTS_TRUNCATE_SIZE);
+        } catch (IOException e) {
+            return "Error reading file contents";
         }
     }
 }
