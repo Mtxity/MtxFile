c57937bdc1e889e34a9bb64bca802a616502bc64
Enrique <edavalos@ucsc.edu>
2026-02-16 02:38:36 -0800

    Integrated HashService into HashController and implemented hash and verify endpoints

--- src/main/java/com/mtxrii/file/mtxfile/api/controller/HashController.java
+++ src/main/java/com/mtxrii/file/mtxfile/api/controller/HashController.java
@@ -1,6 +1,7 @@
 package com.mtxrii.file.mtxfile.api.controller;
 
 import com.mtxrii.file.mtxfile.api.model.Response;
+import com.mtxrii.file.mtxfile.api.service.HashService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hashing")
 public class HashController {
+    private final HashService hashService;
+
+    public HashController(HashService hashService) {
+        this.hashService = hashService;
+    }
 
     @GetMapping("/hash")
     ResponseEntity<Response> handleHash(
             @RequestParam("value") String value
     ) {
-        //
+        return this.hashService.hash(value);
     }
 
     @GetMapping("/verify")
@@ -23,6 +29,6 @@ public class HashController {
             @RequestParam("hash") String hash,
             @RequestParam("value") String value
     ) {
-        //
+        return this.hashService.verify(value, hash);
     }
 }
