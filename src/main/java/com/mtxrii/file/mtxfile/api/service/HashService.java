70a5c2e17587c25a84cb15b75023e27ad77db6dd
Enrique <edavalos@ucsc.edu>
2026-02-16 02:36:36 -0800

    Added hash() and verify() to HashService

--- src/main/java/com/mtxrii/file/mtxfile/api/service/HashService.java
+++ src/main/java/com/mtxrii/file/mtxfile/api/service/HashService.java
@@ -1,16 +1,30 @@
 package com.mtxrii.file.mtxfile.api.service;
 
+import com.mtxrii.file.mtxfile.api.model.HashResponse;
 import com.mtxrii.file.mtxfile.util.HashUtil;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HashService {
 
-    public String hash(String value) {
+    public HashResponse.NewHashResponse hash(String value) {
+        if (value == null || value.isEmpty() || value.equals(" ")) {
+            return new HashResponse.NewHashResponse("Cannot hash empty value");
+        }
+        String hash = this.hashUtil(value);
+        return new HashResponse.NewHashResponse("Successfully hashed input", hash);
+    }
+
+    private String hashUtil(String value) {
         return HashUtil.hashPassword(value);
     }
 
-    public boolean verify(String value, String hash) {
+    public HashResponse.ExistingHashResponse verify(String value, String hash) {
+        boolean matches = this.verifyUtil(value, hash);
+        return new HashResponse.ExistingHashResponse(matches, "String value " + (matches ? "matches" : " does not match") + " hash");
+    }
+
+    private boolean verifyUtil(String value, String hash) {
         return HashUtil.verifyPassword(value, hash);
     }
 }
