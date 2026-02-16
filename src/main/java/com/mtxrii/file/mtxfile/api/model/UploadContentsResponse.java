fcbd50dd96f68d1cf4efc96a2c8d95bdffd9947a
Enrique <edavalos@ucsc.edu>
2026-02-16 01:51:01 -0800

    Added note field to UploadedContentsResponse
    Currently only used to note if file is already uploaded or not

--- src/main/java/com/mtxrii/file/mtxfile/api/model/UploadContentsResponse.java
+++ src/main/java/com/mtxrii/file/mtxfile/api/model/UploadContentsResponse.java
@@ -2,6 +2,7 @@ package com.mtxrii.file.mtxfile.api.model;
 
 public class UploadContentsResponse extends Response {
     public final boolean uploaded;
+    public final String note;
     public final String fileName;
     public final String contentsPreview;
     public final int length;
@@ -10,6 +11,7 @@ public class UploadContentsResponse extends Response {
     public UploadContentsResponse(boolean uploaded, String fileName, String contentsPreview, int length) {
         super(uploaded, uploaded ? 201 : 202);
         this.uploaded = uploaded;
+        this.note = uploaded ? "File already uploaded under this name" : null;
         this.fileName = fileName;
         this.contentsPreview = contentsPreview;
         this.length = length;
@@ -19,6 +21,7 @@ public class UploadContentsResponse extends Response {
     public UploadContentsResponse(boolean uploaded, String fileName, String contentsPreview, int length, boolean passwordProtected) {
         super(uploaded, uploaded ? 201 : 202);
         this.uploaded = uploaded;
+        this.note = uploaded ? "File already uploaded under this name" : null;
         this.fileName = fileName;
         this.contentsPreview = contentsPreview;
         this.length = length;
